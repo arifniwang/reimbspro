a7f494c784836d9fda25e5923a6963bc9860b4ef
Arif Niwang Djati <niwang@crocodic.com>
2019-03-13 21:13:27 +0700

    change tempalte & fix image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,10 @@
 @extends('crudbooster::admin_template')
 @section('content')
-
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet asperiores, aspernatur assumenda atque beatae corporis culpa dignissimos dolorem doloremque doloribus earum enim fugiat id laboriosam possimus sint sunt, suscipit veritatis.
 @endsection
+
+@push('bottom')
+    <script>
+        $('section.content-header').find('small').html('Dashboard')
+    </script>
+@endpush
