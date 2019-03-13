66a519c4ca6a90fc8fe200d3364fbd071359c793
Arif Niwang Djati <niwang@crocodic.com>
2019-03-14 00:36:17 +0700

    update dashbaord

--- app/Http/Controllers/AdminIndexController.php
+++ app/Http/Controllers/AdminIndexController.php
@@ -9,6 +9,172 @@ class AdminIndexController extends \crocodicstudio\crudbooster\controllers\CBCon
 {
     public function getIndex()
     {
-        return view('index');
+        $now = date('Y-m-d H:i:s');
+        $year = date('Y');
+        $month = number_format(date('m'), 0, '', '');
+        $bulan = array(
+            1 => 'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        );
+
+        /**
+         * KUOTA
+         */
+        $master = DB::table('anggaran')
+            ->where('year', $year)
+            ->where('month', $month)
+            ->whereNull('deleted_at')
+            ->first();
+        if (empty($master)) {
+            $anggaran = 0;
+            $sisa = 0;
+            $over = 0;
+        } else {
+            $anggaran = $master->nominal;
+
+            $hitung_sisa = $master->nominal - $master->reimbursement;
+            $sisa = ($hitung_sisa < 0 ? 0 : $hitung_sisa);
+
+            $over = abs(($hitung_sisa < 0 ? $hitung_sisa : 0));
+        }
+
+        /**
+         * PERSENTASE
+         */
+        $persentase = [];
+        $diproses = DB::table('pengajuan')
+            ->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->whereNull('deleted_at')
+            ->where('status', 'Diproses')
+            ->count();
+        $push_persentase['name'] = 'Diproses';
+        $push_persentase['y'] = $diproses;
+        $persentase[] = $push_persentase;
+
+        $disetujui = DB::table('pengajuan')
+            ->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->whereNull('deleted_at')
+            ->where('status', 'Disetujui')
+            ->count();
+        $push_persentase['name'] = 'Disetujui';
+        $push_persentase['y'] = $disetujui;
+        $persentase[] = $push_persentase;
+
+        $ditolak = DB::table('pengajuan')
+            ->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->whereNull('deleted_at')
+            ->where('status', 'Ditolak')
+            ->count();
+        $push_persentase['name'] = 'Ditolak';
+        $push_persentase['y'] = $ditolak;
+        $persentase[] = $push_persentase;
+
+        $persentase = json_encode($persentase);
+
+        /**
+         * GRAFIK
+         */
+        $grafik = [];
+        $grafik_overbudget = [];
+        $grafik_pemakaian = [];
+        $grafik_anggaran = [];
+        $start = strtotime('2019-01-01');
+        $end = strtotime('2019-03-31');
+        while($start < $end)
+        {
+            $year = date('Y', $start);
+            $month = number_format(date('m', $start), 0, '', '');
+            $check_anggaran = DB::table('anggaran')
+                ->whereNull('deleted_at')
+                ->where('year',$year)
+                ->where('month',$month)
+                ->first();
+
+            if (!   empty($check_anggaran)){
+                $check_pemakaian = $check_anggaran->nominal - $check_anggaran->reimbursement;
+                $pemakaian = ($check_pemakaian < 0 ? $check_anggaran->nominal : $check_anggaran->reimbursement);
+                $over = ($check_pemakaian < 0 ? abs($check_pemakaian) : 0);
+
+                $grafik_anggaran[] = $check_anggaran->nominal;
+                $grafik_pemakaian[] = $pemakaian;
+                $grafik_overbudget[] = $over;
+            }else{
+                $grafik_anggaran[] = 0;
+                $grafik_pemakaian[] = 0;
+                $grafik_overbudget[] = 0;
+            }
+
+            $start = strtotime("+1 month", $start);
+        }
+        $push_grafik['type'] = 'column';
+        $push_grafik['name'] = 'Over Budget';
+        $push_grafik['data'] = $grafik_overbudget;
+        $grafik[] = $push_grafik;
+
+        $push_grafik['type'] = 'column';
+        $push_grafik['name'] = 'Pemakaian';
+        $push_grafik['data'] = $grafik_pemakaian;
+        $grafik[] = $push_grafik;
+
+        $push_grafik['type'] = 'spline';
+        $push_grafik['name'] = 'Anggaran';
+        $push_grafik['data'] = $grafik_anggaran;
+        $grafik[] = $push_grafik;
+
+        $grafik = json_encode($grafik);
+
+        /**
+         * TABLE
+         */
+        $disetujui = DB::table('pengajuan')
+            ->join('users', 'users.id', '=', 'pengajuan.id_users')
+            ->whereNull('pengajuan.deleted_at')
+            ->where('pengajuan.status', 'Disetujui')
+            ->count();
+        $ditolak = DB::table('pengajuan')
+            ->join('users', 'users.id', '=', 'pengajuan.id_users')
+            ->whereNull('pengajuan.deleted_at')
+            ->where('pengajuan.status', 'Ditolak')
+            ->count();
+        $last_disetujui = DB::table('pengajuan')
+            ->join('users', 'users.id', '=', 'pengajuan.id_users')
+            ->select('pengajuan.*', 'users.name as nama_user')
+            ->whereNull('pengajuan.deleted_at')
+            ->where('pengajuan.status', 'Disetujui')
+            ->orderBy('pengajuan.id', 'DESC')
+            ->limit(10)
+            ->get();
+        $last_ditolak = DB::table('pengajuan')
+            ->join('users', 'users.id', '=', 'pengajuan.id_users')
+            ->select('pengajuan.*', 'users.name as nama_user')
+            ->whereNull('pengajuan.deleted_at')
+            ->where('pengajuan.status', 'Ditolak')
+            ->orderBy('pengajuan.id', 'DESC')
+            ->limit(10)
+            ->get();
+
+        $response['anggaran'] = $anggaran;
+        $response['sisa'] = $sisa;
+        $response['over'] = $over;
+        $response['persentase'] = $persentase;
+        $response['grafik'] = $grafik;
+        $response['disetujui'] = $disetujui;
+        $response['ditolak'] = $ditolak;
+        $response['last_disetujui'] = $last_disetujui;
+        $response['last_ditolak'] = $last_ditolak;
+        return view('index', $response);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,325 @@
 @extends('crudbooster::admin_template')
 @section('content')
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet asperiores, aspernatur assumenda atque beatae corporis culpa dignissimos dolorem doloremque doloribus earum enim fugiat id laboriosam possimus sint sunt, suscipit veritatis.
+    <style>
+        section.content-header small {
+            display: none;
+        }
+
+        .box-header {
+            border-bottom: 1px solid #CCC;
+            font-size: 16px;
+            font-weight: bold;
+            padding-left: 0;
+            padding-right: 0;
+        }
+
+        .info-box {
+            border-radius: 5px;
+            padding: 10px 20px;
+        }
+
+        .info-box .separator-badge {
+            width: 50px;
+            border-radius: 10px;
+            height: 10px;
+            margin-bottom: 25px;
+        }
+
+        .info-box .separator-badge.bg-success {
+            background-color: #2DC399;
+        }
+
+        .info-box .separator-badge.bg-warning {
+            background-color: #FFD31F;
+        }
+
+        .info-box .separator-badge.bg-danger {
+            background-color: #E75651;
+        }
+
+        .info-box .info-title {
+            color: #444 !important;
+            font-size: 16px;
+            font-weight: bold;
+        }
+
+        .info-box .info-nominal {
+            color: #444 !important;
+            font-size: 28px;
+            font-weight: 400;
+        }
+
+        .table-box {
+            border-radius: 5px;
+            background-color: #FFFFFF;
+        }
+
+        .table-title-box-success {
+            background-color: #2DC399;
+            color: #FFFFFF;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+            padding: 20px 20px 10px;
+        }
+
+        .table-title-box-danger {
+            background-color: #E75651;
+            color: #FFFFFF;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+            padding: 20px 20px 10px;
+        }
+
+        .table-content-box {
+            border-bottom-left-radius: 5px;
+            border-bottom-right-radius: 5px;
+            padding: 10px 0px 20px;
+        }
+
+        .table-box-header {
+            font-size: 20px;
+        }
+
+        .table-box-content {
+            font-size: 28px;
+            font-family: bold;
+        }
+
+        .table-box-icon {
+            font-size: 50px;
+            opacity: 0.5;
+        }
+    </style>
+
+    <div class="row" style="margin-top: 10px;">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="info-box shadow">
+                <p class="info-title">Total Anggaran Bulan Ini</p>
+                <div class="separator-badge bg-success"></div>
+                <p class="info-nominal">Rp.{{number_format($anggaran,0,'.','.')}},-</p>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="info-box shadow">
+                <p class="info-title">Sisa Anggaran Bulan Ini</p>
+                <div class="separator-badge bg-warning"></div>
+                <p class="info-nominal">Rp.{{number_format($sisa,0,'.','.')}},-</p>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="info-box shadow">
+                <p class="info-title">Over Budget Bulan Ini</p>
+                <div class="separator-badge bg-danger"></div>
+                <p class="info-nominal">Rp.{{number_format($over,0,'.','.')}},-</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 20px;">
+        <div class="col-md-5 col-sm-5 col-xs-12">
+            <div class="info-box shadow">
+                <div class="box-header">
+                    Persentase Pengajuan
+                </div>
+                <div class="box-body">
+                    <div id="persentasePengajuan" style="min-width: 100%; height: 300px;"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-7 col-sm-7 col-xs-12">
+            <div class="info-box shadow">
+                <div class="box-header">
+                    Grafik Pengajuan
+                </div>
+                <div class="box-body">
+                    <div id="pengajuanTahunan" style="min-width: 100%; height: 300px;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 20px;">
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="table-box shadow">
+                <div class="table-title-box-success">
+                    <span class="pull-right table-box-icon"><i class="fa fa-user-plus"></i></span>
+                    <p class="table-box-header">Total Pengajuan yang Diterima</p>
+                    <p class="table-box-content">{{$disetujui}}</p>
+                </div>
+                <div class="table table-content-box">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Tanggal Pengajuan</th>
+                            <th>Nominal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($last_disetujui as $row)
+                            <tr>
+                                <td style="font-size: 13px;">{{$row->nama_user}}</td>
+                                <td style="font-size: 13px;">{{ date('d M Y',strtotime($row->created_at)) }}</td>
+                                <td style="font-size: 13px;">Rp.{{number_format($row->total_nominal,0,'.','.')}},-</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="table-box shadow">
+                <div class="table-title-box-danger">
+                    <span class="pull-right table-box-icon"><i class="fa fa-user-times"></i></span>
+                    <p class="table-box-header">Total Pengajuan yang Ditolak</p>
+                    <p class="table-box-content">{{$ditolak}}</p>
+                </div>
+                <div class="table table-content-box">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Tanggal Pengajuan</th>
+                            <th>Nominal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($last_ditolak as $row)
+                            <tr>
+                                <td style="font-size: 13px;">{{$row->nama_user}}</td>
+                                <td style="font-size: 13px;">{{ date('d M Y',strtotime($row->created_at)) }}</td>
+                                <td style="font-size: 13px;">Rp.{{number_format($row->total_nominal,0,'.','.')}},-</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('bottom')
     <script>
         $('section.content-header').find('small').html('Dashboard')
+        $('section.content-header').find('small').show()
     </script>
+
+    {{--HIGHCHART PLUGIN--}}
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    {{--<script src="https://code.highcharts.com/modules/exporting.js"></script>--}}
+    {{--<script src="https://code.highcharts.com/modules/export-data.js"></script>--}}
+
+    <script>
+        /**
+         * PERSENTASE PENGAJUAN
+         */
+        Highcharts.chart('persentasePengajuan', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            style: {
+                color: 'white',
+                fontFamily: 'Helvetica',
+                border: 0
+            },
+            colors: ['#FFD31F', '#2DC399', '#E75651'],
+            plotOptions: {
+                pie: {
+                    dataLabels: {
+                        enabled: true,
+                        distance: -50,
+                        format: '<b>{point.name}</b>:<br> {point.y} ({point.percentage:.0f}%)',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        },
+                        style: {
+                            color: 'white',
+                            fontFamily: 'Helvetica',
+                            border: 0
+                        }
+                    },
+                    cursor: 'pointer',
+                    showInLegend: true,
+                    size: '100%'
+                }
+            },
+            series: [{
+                name: 'Share',
+                data: {!! $persentase !!}
+            }]
+        });
+
+
+        /**
+         * GRAFIK TAHUNAN
+         */
+        Highcharts.chart('pengajuanTahunan', {
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['Januari', 'Febuari', 'Maret', 'April', 'Mei']
+            },
+            credits: {
+                enabled: false
+            },
+            yAxis: {
+                allowDecimals: false,
+                min: 0,
+                title: {
+                    text: ''
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal'
+                },
+                dataLabels: {
+                    enabled: true
+                },
+                showInLegend: true,
+            },
+            colors: ['#E75651', '#2DC399', '#FFD31F'],
+            series: {!! $grafik !!}
+            // series: [{
+            //     type: 'column',
+            //     name: 'Over Budget',
+            //     data: [1000000, 0, 500000, 0, 15000]
+            // }, {
+            //     type: 'column',
+            //     name: 'Pemakaian',
+            //     data: [5000000, 0, 3500000, 3000000, 1000000]
+            // }, {
+            //     type: 'spline',
+            //     name: 'Anggaran',
+            //     data: [5000000, 3000000, 3500000, 4000000, 1000000],
+            //     marker: {
+            //         lineWidth: 2,
+            //         lineColor: Highcharts.getOptions().colors[3],
+            //         fillColor: 'white'
+            //     }
+            // }]
+        });
+    </script>
+
 @endpush
